1b8f9259876b94d5ceb123887b1021863721926b
Shadman Choudhury <choudhs2@bxscience.edu>
2023-12-18 19:30:06 -0500

    Make the sidebar look better, changed sidebar links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 import './Sidebar.css'; 
 
 export default function Sidebar() {
+    //Todo: make it relative link paths, ie. if in Anime tab, link to Anime/search, etc
     return (
         <div className="Sidebar">
             <ul>
                 <li><Link to="/search">Add to your list!</Link></li>
-                <li><Link to="/Review">Review something on your list!</Link></li>
                 <li><Link to="/suggestions">Discover something New!</Link></li>
             </ul>
         </div>
